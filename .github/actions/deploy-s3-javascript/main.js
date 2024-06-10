a1dcaa37af4dd9bb7a6f2c88cfd5249d4be9e2a2
Micha <micha@test.com>
2024-06-10 15:29:14 +0200

    use GitHub JS actions libs

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,1 +1,9 @@
-console.log("Deploying ...")
+import core from "@actions/core"
+import github from "@actions/github"
+import exec from "@actions/exec"
+
+function run() {
+    core.notice('Hello from my custom JavaScript action');
+}
+
+run();
